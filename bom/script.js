5312e9676a824aec5efd9616496b18f5c488ba31
Nevertheless <swgu931@chol.com>
2021-07-04 13:57:12 +0900

    Add lecture note and so on

--- bom/script.js
+++ bom/script.js
@@ -1,4 +1,13 @@
-var hw = document.getElementById('hw');
-hw.addEventListener('click', function(){
-	 alert('Hello world');
-})
+<p>time : <span id="time"></span></p>
+<input type="button" id="execute" value="execute" />
+<script src="//code.jquery.com/jquery-1.11.0.min.js"></script>
+<script>
+ $('#execute').click(function(){
+ $.ajax({
+ url:'./time.php',
+ success:function(data){
+ $('#time').append(data);
+ }
+ })
+ })
+</script>
